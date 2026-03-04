88960183389cd436ecb87f8b7e3e8d83f052a8d4
umjunwon-sys <umjunwon@gmail.com>
2026-03-05 00:27:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'timebox-v1';
-const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'timebox-v2';
+const ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
